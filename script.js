edacdec722f30e4e5cce1d9212a92e7899b6eefc
Omar Mahmoudi <mahmoudiiomarr@gmail.com>
2026-07-28 20:16:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -553,3 +553,9 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log("Error: Toggle button or Sidebar not found!");
     }
 });
+const sidebar = document.querySelector('.sidebar');
+const toggleBtn = document.getElementById('toggleSidebar'); // تأكد إن الـ ID صحيح
+
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
